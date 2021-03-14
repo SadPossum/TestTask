cc16e55ba8f0258969e3cbf720a3e65088cded38
SadPossum <61198926+SadPossum@users.noreply.github.com>
2021-03-14 13:14:56 +0300

    Test task release v1.0

---
+++ BackendProject/BackendProject/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackendProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            string[] lines =
+{
+            "Lorem ipsum dolor sit amet.",
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, similique?",
+            "Lorem ipsum dolor sit amet consectetur."
+            };
+
+            Thread.Sleep(3000);
+
+            return lines;
+        }
+    }
+}
---
+++ frontend-project/pages/index.vue
@@ -0,0 +1,72 @@
+<template>
+
+  <v-card id="temp-card-class">
+
+    <v-subheader>
+      <v-btn id="temp-button-class" @click="loadData()"> Типа кнопка </v-btn>
+    </v-subheader>
+
+    <v-progress-circular
+      id="temp-progress-circular-class"
+      indeterminate
+      v-if="dataLoading"
+    />
+
+    <v-list disabled>
+      <v-list-item-group>
+        <v-list-item v-for="(item, i) in items" :key="i">
+          <v-list-item-content>
+            <v-list-item-title v-text="item"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+
+  </v-card>
+
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: () => ({
+    selectedItem: 1,
+    items: ["ddsfrsd"],
+    dataLoading: false
+  }),
+
+  methods: {
+    loadData: async function () {
+      this.dataLoading = true
+      this.items = []
+
+      await axios
+        .get('http://localhost:5000/api/Values')
+        .then(response => this.items = response.data)
+        .catch(err => console.log(err));
+
+      this.dataLoading = false
+    },
+  },
+};
+</script>
+
+<style>
+#temp-card-class {
+  height: 300px;
+  width: 500px;
+  display: block;
+  margin: auto;
+}
+#temp-progress-circular-class {
+  display: block;
+  margin: auto;
+  top: 30%;
+}
+#temp-button-class {
+  display: block;
+  margin-left: 20px;
+  margin-top: 20px;
+}
+</style>
